1757fea068de24ebb1f6bf12afe6dd691ad41310
Arie Litovsky <arie.litovsky@gmail.com>
2012-09-14 12:12:13 -0500

    Changed config file

--- MKStoreManager.m
+++ MKStoreManager.m
@@ -218,7 +218,7 @@ static MKStoreManager* _sharedStoreManager;
 {
   return [NSDictionary dictionaryWithContentsOfFile:
           [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:
-           @"MKStoreKitConfigs.plist"]];
+           @"TTStoreKitConfigs.plist"]];
 }
 
 - (void) restorePreviousTransactionsOnComplete:(void (^)(void)) completionBlock
